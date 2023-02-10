1d69256567294bad44ba8b27321bd21927d1f636
Mark Berkey <111755911+mbrky6@users.noreply.github.com>
2023-02-10 00:30:59 +0000

    Changed the fetchQuotes function to read and display quotes from zyBooks instead of filler data.

--- quote.js
+++ quote.js
@@ -1,25 +1,32 @@
-window.addEventListener("DOMContentLoaded", function () {
-   document.querySelector("#fetchQuotesBtn").addEventListener("click", function () {
+window.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("#fetchQuotesBtn").addEventListener("click", function() {
 
-      // Get values from drop-downs
-      const topicDropdown = document.querySelector("#topicSelection");
-      const selectedTopic = topicDropdown.options[topicDropdown.selectedIndex].value;
-      const countDropdown = document.querySelector("#countSelection");
-      const selectedCount = countDropdown.options[countDropdown.selectedIndex].value;
-   
-      // Get and display quotes
-      fetchQuotes(selectedTopic, selectedCount);	   
-   });
+    // Get values from drop-downs
+    const topicDropdown = document.querySelector("#topicSelection");
+    const selectedTopic = topicDropdown.options[topicDropdown.selectedIndex].value;
+    const countDropdown = document.querySelector("#countSelection");
+    const selectedCount = countDropdown.options[countDropdown.selectedIndex].value;
+
+    // Get and display quotes
+    fetchQuotes(selectedTopic, selectedCount);
+  });
 });
 
 // TODO: Modify to use Fetch API
-function fetchQuotes(topic, count) {
-   
-   let html = "<ol>";
-   for (let c = 1; c <= count; c++) {
-      html += `<li>Quote ${c} - Anonymous</li>`;
-   }
-   html += "</ol>";
+async function fetchQuotes(topic, count) {
+  let url = "https://wp.zybooks.com/quotes.php?topic=" + topic + "&count=" + count;
+  let response = await fetch(url);
+  
+  if (response.ok) {
+    let quotes = await response.json();
 
-   document.querySelector("#quotes").innerHTML = html;
+    let html = "";
+    html = "<ol>";
+    for (let c of quotes) {
+      html += `<li>${c.quote} - ${c.source}</li>`;
+    } // for (number requested)
+    html += "</ol>";
+  
+    document.querySelector("#quotes").innerHTML = html;
+  } // if (fetch successful)
 }
